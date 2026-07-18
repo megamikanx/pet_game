dc51c054737f8e8dc5a5eacd42bdc72b9119f385
DylanWotherspoon <dylan.wotherspoon.au@gmail.com>
2026-07-19 02:54:05 +1000

    Added in extra graphics, however are not implemented

--- scripts/stageinfo_scripts/info_stage.gd
+++ scripts/stageinfo_scripts/info_stage.gd
@@ -5,7 +5,14 @@ extends Resource
 
 @export var stageNum = 1
 @export var NUM_PETS = 10
-@export var petTexture: Texture2D = preload("res://sprites/pet_sprites/pibbles.png")
+@export var levelTexture: Texture2D
+@export var petTexture: Texture2D
+
+@export var clientTextureTalkClose: Texture2D
+@export var clientTextureTalkOpen: Texture2D
+
+@export var clientTexture: Texture2D
+@export var clientTexture: Texture2D
 
 @export var answer_personality: Array[ItemInfo.ITEM_TYPE]
 @export var valid_preferences: Array[ItemInfo.ITEM_TYPE]
