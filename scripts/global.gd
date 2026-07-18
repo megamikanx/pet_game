e0d5baecb09af44a0e95d3188a71517a6060ff3f
Stephanie <seoyoung.min524@gmail.com>
2026-07-18 20:48:23 +0900

    added stage 1-5 images for clue and packing

--- scripts/global.gd
+++ scripts/global.gd
@@ -11,7 +11,6 @@ var has_seen_comic: bool = false
 var stage_data: Dictionary = {
 	1: {
 		"client_name": "Miku",
-		"bag_limit": 1,
 		"portrait_closed": preload(
 			"res://sprites/miku_talk_close.png"
 		),
@@ -27,8 +26,105 @@ var stage_data: Dictionary = {
 			preload("res://sprites/miku_talk_6.png"),
 		],
 		"required_items": ["Plushie"],
-		"wrong_line": "Miku needs you to lock in.",
-		"success_line": "San-Kyu!",
+		"wrong_line": preload("res://sprites/miku_level_wrong.png"),
+		"success_line": preload("res://sprites/miku_level_right.png"),
+	},
+	2: {
+		"client_name": "Sonic",
+		"portrait_closed": preload(
+			"res://sprites/sonic_talk_close.png"
+		),
+		"portrait_open": preload(
+			"res://sprites/sonic_talk_open.png"
+		),
+		"dialogue": [
+			preload("res://sprites/sonic_talk_1.png"),
+			preload("res://sprites/sonic_talk_2.png"),
+			preload("res://sprites/sonic_talk_3.png"),
+			preload("res://sprites/sonic_talk_4.png"),
+			preload("res://sprites/sonic_talk_5.png"),
+			preload("res://sprites/sonic_talk_6.png"),
+			preload("res://sprites/sonic_talk_7.png"),
+			preload("res://sprites/sonic_talk_8.png"),
+			preload("res://sprites/sonic_talk_9.png"),
+		],
+		"required_items": ["TopHat", "Water"],
+		"wrong_line": preload("res://sprites/sonic_level_wrong.png"),
+		"success_line": preload("res://sprites/sonic_level_right.png"),
+	},
+	3: {
+		"client_name": "Kris",
+		"portrait_closed": preload(
+			"res://sprites/kris_talk_close.png"
+		),
+		"portrait_open": preload(
+			"res://sprites/kris_talk_open.png"
+		),
+		"dialogue": [
+			preload("res://sprites/kris_talk_1.png"),
+			preload("res://sprites/kris_talk_2.png"),
+			preload("res://sprites/kris_talk_3.png"),
+			preload("res://sprites/kris_talk_4.png"),
+			preload("res://sprites/kris_talk_5.png"),
+			preload("res://sprites/kris_talk_6.png"),
+			preload("res://sprites/kris_talk_7.png"),
+			preload("res://sprites/kris_talk_8.png"),
+			preload("res://sprites/kris_talk_9.png"),
+			preload("res://sprites/kris_talk_10.png"),
+		],
+		"required_items": ["Speaker", "BucketSlop", "Flower", "DollarBill"],
+		"wrong_line": preload("res://sprites/kris_level_wrong.png"),
+		"success_line": preload("res://sprites/kris_level_right.png"),
+	},
+	4: {
+		"client_name": "Joker",
+		"portrait_closed": preload(
+			"res://sprites/joker_talk_close.png"
+		),
+		"portrait_open": preload(
+			"res://sprites/joker_talk_open.png"
+		),
+		"dialogue": [
+			preload("res://sprites/joker_talk_1.png"),
+			preload("res://sprites/joker_talk_2.png"),
+			preload("res://sprites/joker_talk_3.png"),
+			preload("res://sprites/joker_talk_4.png"),
+			preload("res://sprites/joker_talk_5.png"),
+			preload("res://sprites/joker_talk_6.png"),
+			preload("res://sprites/joker_talk_7.png"),
+			preload("res://sprites/joker_talk_8.png"),
+			preload("res://sprites/joker_talk_9.png"),
+			preload("res://sprites/joker_talk_10.png"),
+			preload("res://sprites/joker_talk_11.png"),
+		],
+		"required_items": ["Bath", "Burger", "Beer", "Squishy"],
+		"wrong_line": preload("res://sprites/joker_level_wrong.png"),
+		"success_line": preload("res://sprites/joker_level_right.png"),
+	},
+	5: {
+		"client_name": "Isaac",
+		"portrait_closed": preload(
+			"res://sprites/isaac_talk_close.png"
+		),
+		"portrait_open": preload(
+			"res://sprites/isaac_talk_open.png"
+		),
+		"dialogue": [
+			preload("res://sprites/isaac_talk_1.png"),
+			preload("res://sprites/isaac_talk_2.png"),
+			preload("res://sprites/isaac_talk_3.png"),
+			preload("res://sprites/isaac_talk_4.png"),
+			preload("res://sprites/isaac_talk_5.png"),
+			preload("res://sprites/isaac_talk_6.png"),
+			preload("res://sprites/isaac_talk_7.png"),
+			preload("res://sprites/isaac_talk_8.png"),
+			preload("res://sprites/isaac_talk_9.png"),
+			preload("res://sprites/isaac_talk_10.png"),
+			preload("res://sprites/isaac_talk_11.png"),
+		],
+		"required_items": ["Bath", "ProteinBar", "Beer", "Plushie", "TopHat"],
+		"wrong_line": preload("res://sprites/isaac_level_wrong.png"),
+		"success_line": preload("res://sprites/isaac_level_right.png"),
 	},
 }
 
