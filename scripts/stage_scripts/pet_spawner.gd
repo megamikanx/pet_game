afa3f52a0a6b980ec53a86c60416f6ea6be6f5a6
DylanWotherspoon <dylan.wotherspoon.au@gmail.com>
2026-07-18 23:07:19 +1000

    Improved pick up of pets

--- scripts/stage_scripts/pet_spawner.gd
+++ scripts/stage_scripts/pet_spawner.gd
@@ -5,14 +5,12 @@ const OFFSET = 64
 
 signal hold_pet
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var collection = get_parent().get_node("Collection")
 
 func spawn_pets(num : int, texture: Texture2D) -> void:
 	var offsetX = texture.get_size().x * 0.5 * Global.petScale
 	var offsetY = texture.get_size().y * 0.5 * Global.petScale
-	print(offsetX)
+
 	var lower_sx = 421 + offsetX
 	var upper_sx = 941 - offsetX
 	var lower_sy = 78 + offsetY
@@ -21,7 +19,12 @@ func spawn_pets(num : int, texture: Texture2D) -> void:
 	var instance: Pet
 	for i in num:
 		instance = Global.PET_SCENE.instantiate()
+		instance.pick_me_up.connect(pet_picked_up)
+		instance.pick_me_up.connect(collection.collected_pickedup)
 		instance.freeze_pets.connect(freeze_pets)
+		instance.unfreeze_pets.connect(unfreeze_pets)
+		instance.send_coll.connect(collection.place_pet)
+		
 		add_child(instance)
 		
 		instance.position = Vector2(randi_range(lower_sx, upper_sx),
@@ -29,19 +32,14 @@ func spawn_pets(num : int, texture: Texture2D) -> void:
 		instance.set_petTexture(texture)
 		
 	pass
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
-func _input(event: InputEvent) -> void:
-	pass
-
-func freeze_pets(the_holded : Pet) -> void:
+func pet_picked_up(the_holded : Pet) -> void:
 	hold_pet.emit(the_holded)
+
+func freeze_pets() -> void:
 	for pet in get_children():
 		pet.freeze()
 
-func unfreeze_pets(the_holded : Pet) -> void:
+func unfreeze_pets() -> void:
 	for pet in get_children():
 		pet.unfreeze.call_deferred()
